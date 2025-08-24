35e37cd1575b40f605d86fb513740227be726e54
nicod <nicolas.deforge@ouihelp.fr>
2025-08-24 23:38:12 +0200

    Hello world + primitives

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut x = 5;
+
+    println!("x is {x}");
+
+    x = 20;
+
+    println!("x is {x}");
+
+    let integer: i32 = 10;
+    let float = 10.12;
+    let is_true = true;
+    let test = 'a';
+
+    println!("integer is {integer}");
+    println!("float is {float}");
+    println!("is_true is {is_true}");
+    println!("test is {test}");
+}
